433a4c9e1be2fe07295bfd7da16b8e67ce3940a2
Andrey Felipe de Lima <andreylima@alunos.utfpr.edu.br>
2026-09-21 16:04:55 -0300

    feat(router): registra rotas de login e usuarios

--- public/index.php
+++ public/index.php
@@ -5,10 +5,20 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 use App\Core\Router;
+use App\Controllers\AuthController;
+use App\Controllers\UsuarioController;
 
 $router = new Router();
 
-// Rotas serão registradas aqui pelos controllers
-// Exemplo: $router->get('/login', [App\Controllers\AuthController::class, 'showLogin']);
+$router->get('/login', [AuthController::class, 'showLogin']);
+$router->post('/login', [AuthController::class, 'login']);
+$router->post('/logout', [AuthController::class, 'logout']);
+
+$router->get('/usuarios', [UsuarioController::class, 'index']);
+$router->get('/usuarios/novo', [UsuarioController::class, 'create']);
+$router->post('/usuarios/novo', [UsuarioController::class, 'store']);
+$router->get('/usuarios/editar', [UsuarioController::class, 'edit']);
+$router->post('/usuarios/editar', [UsuarioController::class, 'update']);
+$router->post('/usuarios/inativar', [UsuarioController::class, 'inativar']);
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
