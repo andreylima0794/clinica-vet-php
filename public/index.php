006ad6eb29e939149040a97ef3bb4a8ea72df5cf
DMB29 <110937863+DMB29@users.noreply.github.com>
2026-09-24 15:25:34 -0300

    feat(router): registra rotas de tutores, animais e consultas

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ session_start();
 use App\Core\Router;
 use App\Controllers\AuthController;
 use App\Controllers\UsuarioController;
+use App\Controllers\TutorController;
+use App\Controllers\AnimalController;
+use App\Controllers\ConsultaController;
 
 set_error_handler(function ($severity, $message, $file, $line) {
     throw new \ErrorException($message, 0, $severity, $file, $line);
@@ -32,4 +35,23 @@ $router->get('/usuarios/editar', [UsuarioController::class, 'edit']);
 $router->post('/usuarios/editar', [UsuarioController::class, 'update']);
 $router->post('/usuarios/inativar', [UsuarioController::class, 'inativar']);
 
+$router->get('/tutores', [TutorController::class, 'index']);
+$router->get('/tutores/novo', [TutorController::class, 'novo']);
+$router->post('/tutores/novo', [TutorController::class, 'criar']);
+$router->get('/tutores/editar', [TutorController::class, 'editar']);
+$router->post('/tutores/editar', [TutorController::class, 'atualizar']);
+
+$router->get('/animais', [AnimalController::class, 'index']);
+$router->get('/animais/novo', [AnimalController::class, 'novo']);
+$router->post('/animais/novo', [AnimalController::class, 'criar']);
+$router->get('/animais/editar', [AnimalController::class, 'editar']);
+$router->post('/animais/editar', [AnimalController::class, 'atualizar']);
+
+$router->get('/consultas', [ConsultaController::class, 'index']);
+$router->get('/consultas/novo', [ConsultaController::class, 'novo']);
+$router->post('/consultas/novo', [ConsultaController::class, 'criar']);
+$router->get('/consultas/editar', [ConsultaController::class, 'editar']);
+$router->post('/consultas/editar', [ConsultaController::class, 'atualizar']);
+
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
